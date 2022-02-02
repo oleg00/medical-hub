3843e9053480b2c600a4592b4f5229b44bfd7f46
User <user@gmail.com>
2022-02-02 21:14:08 +0200

    Doctor ref on contact has been added

--- MedicalHub.DAL/Doctor.cs
+++ MedicalHub.DAL/Doctor.cs
@@ -3,6 +3,8 @@
     public class Doctor
     {
         public int Id { get; set; }
+        public Contact Contact { get; set; }
+        public int ContactId { get; set; }
         public List<Education> Educations { get; set; }
         public List<DoctorCategory> DoctorCategories { get; set; }
         public List<DoctorFeedback> DoctorFeedbacks { get; set; }
