9c709c930e6d0a8e3acc97ee23e8eec857c19b46
oleg00 <radchenko.ov00@gmail.com>
2022-02-02 21:23:16 +0200

    Update ServiceFeedback.cs

--- MedicalHub.DAL/ServiceFeedback.cs
+++ MedicalHub.DAL/ServiceFeedback.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MedicalHub.DAL
+﻿namespace MedicalHub.DAL
 {
-    class ServiceFeedback
+    public class ServiceFeedback
     {
         public int Id { get; set; }
         public Contact Contact { get; set; }
