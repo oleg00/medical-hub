7c3db9f3e36b64cefa1522dca11bdab08cc0abd2
User <user@gmail.com>
2022-02-02 20:47:38 +0200

    Minor fix

--- MedicalHub.DAL/DoctorFeedback.cs
+++ MedicalHub.DAL/DoctorFeedback.cs
@@ -3,7 +3,9 @@
     public class DoctorFeedback
     {
         public int Id { get; set; }
+        public Patient Patient { get; set; }
         public int PatientId { get; set; }
+        public Doctor Doctor { get; set; }
         public int DoctorId { get; set; }
         public double Rate { get; set; }
         public string FeedbackMessage { get; set; }
