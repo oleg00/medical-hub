99f8767c9ddc0c36ea85e90675a8623915153b35
oleg00 <radchenko.ov00@gmail.com>
2022-02-02 21:23:46 +0200

    Update Patient.cs

--- MedicalHub.DAL/Patient.cs
+++ MedicalHub.DAL/Patient.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MedicalHub.DAL
 {
-    class Patient
+    public class Patient
     {
         public int Id { get; set; }
         public Contact Contact { get; set; }
