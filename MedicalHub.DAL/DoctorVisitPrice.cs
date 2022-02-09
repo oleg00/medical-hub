7943273b0fe1c1d01cb7895f22bef27b0f0157e7
User <user@gmail.com>
2022-02-09 17:56:36 +0200

    Minor model fixes

---
+++ MedicalHub.DAL/DoctorVisitPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalHub.DAL
+{
+    public class DoctorVisitPrice
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public int DoctorCategoryId { get; set; }
+        public DoctorCategory DoctorCategory { get; set; }
+    }
+}
