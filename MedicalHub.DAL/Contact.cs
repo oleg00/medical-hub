ea36297f56f91b51910e1522fa8d04b91cd5de64
Oleg Radchenko <radchenko.ov00@gmail.com>
2022-02-15 20:56:34 +0200

    ContactType and nullables to the properties in a Thread added

--- MedicalHub.DAL/Contact.cs
+++ MedicalHub.DAL/Contact.cs
@@ -8,5 +8,7 @@ namespace MedicalHub.DAL
         public string PhoneNumber { get; set; }
         public List<ContactMessenger> ContactMessengers { get; set; }
         public string ImageUrl { get; set; }
+        public ContactType ContactType { get; set; }
+        public int ContactTypeId { get; set; }
     }
 }
